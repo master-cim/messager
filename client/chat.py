ebb006700751d16735853e3ea9f8810f8afd618e
Svetlana <master@sevsu.energynet.ru>
2022-10-18 17:05:57 +0300

    сделала мессенджер который записывает сообщения в

--- client/chat.py
+++ client/chat.py
@@ -6,6 +6,7 @@ send_message = document.getElementById("send_message")
 sender = document.getElementById("sender")
 message_text = document.getElementById("message_text")
 chat_window = document.getElementById("chat_window")
+emoji_selector = document.getElementById("emoji_selector")
 
 # Добавляет новое сообщение в список сообщений
 def append_message(message):
@@ -19,7 +20,8 @@ def append_message(message):
 # Вызывается при клике на send_message
 async def send_message_click(e):
     # Отправляем запрос
-    await fetch(f"/send_message?sender={sender.value}&text={message_text.value}", method="GET")
+    sender_full_name = emoji_selector.value + sender.value
+    await fetch(f"/send_message?sender={sender_full_name}&text={message_text.value}", method="GET")
     # Очищаем поле
     message_text.value = ""
 
@@ -27,7 +29,8 @@ async def send_message_click(e):
 # Загружает новые сообщения с сервера и отображает их
 async def load_fresh_messages():
     global last_seen_id
-    # 1. Загружать все сообщения каждую секунду (большой трафик)
+    # 1 БЫЛО. Загружать все сообщения каждую секунду (большой трафик)
+    # 2 СТАЛО. Загружать только новые сообщения
     result = await fetch(f"/get_messages?after={last_seen_id}", method="GET")  # Делаем запрос
     # chat_window.innerHTML = ""  # Очищаем окно с сообщениями
     data = await result.json()
@@ -36,10 +39,19 @@ async def load_fresh_messages():
         last_seen_id = msg["msg_id"]  # msg_id Последнего сообщение
         append_message(msg)
     set_timeout(1, load_fresh_messages) # Запускаем загрузку заново через секунду
-    # 2. Загружать только новые сообщения
+
+
+async def message_keypress(event):
+    # console.log(event) # Посмотреть нажатие клавишь  - это для отладки
+    # вконсоли браузера
+    # event.keyCode
+    if event.code == 'Enter' or event.code == 'NumpadEnter':
+        await send_message_click(event)
 
 
 # Устнаваливаем действие при клике
 send_message.onclick = send_message_click
-append_message({"sender":"MIKE", "text":"Add Message Works!!!", "time": "25:88"})
+# Устанавливаем действие при нажатии клавиши
+message_text.onkeypress = message_keypress
+append_message({"sender":"КИТ", "text":"Пирвет всем участникам чата!!!", "time": "88:88"})
 load_fresh_messages()
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from flask import Flask, request, render_template
 from datetime import datetime
+import json
 
 app = Flask(__name__, static_folder="./client", template_folder="./client")  # Настройки приложения
 
 msg_id = 1
-all_messages = []
+# all_messages = [] #Без использованияфайла db.json
+DB_FILE = "db.json"
+
+def load_messages():
+    with open(DB_FILE, "r") as json_file: #Открываем файл для чтения "r"
+        data = json.load(json_file)
+        return data["messages"]
+
+
+all_messages = load_messages() # Список сообщений,которыйзагрузитьсяизфайла
+
+
+def save_messages():
+    with open(DB_FILE, "w") as json_file: #Открываем файл для записи "w"
+        data = {"messages": all_messages}
+        json.dump(data, json_file) #Сохраняет данные в файл
 
 
 @app.route("/chat")
@@ -22,10 +38,16 @@ def add_message(sender, text):
     }
     msg_id += 1
     all_messages.append(new_message)
+    save_messages()    # Сохраняем каждое сообщение
+    # if msg_id % 10 == 0:
+        # save_messages() #Сохраняем все сообщения регулярно
 
+    #ToDo: Сохранять сообщения каждые 5 секунд, acycio
+    #ToDo: Сохранять сообщения без перезаписи файла, адописывать в конец файла построчно open(file, "a")
+    
 
 # API для получения списка сообщений
-# Только новые сообщения: /get_messages?after=5
+# Только 5 новых сообщений: /get_messages?after=5
 @app.route("/get_messages")
 def get_messages():
     after = int(request.args["after"])
@@ -48,4 +70,8 @@ def hello_page():
     return "New text goes here"
 
 
-app.run()
+# #Приложение запускается локально на порту 5000
+# app.run()
+
+# Запуск на всех доступных интерфейсах
+app.run(host="0.0.0.0", port=80)
